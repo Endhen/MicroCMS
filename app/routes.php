4f615b9c507bfd84b22a068e2f0a942581147f7d
Endhen <bogou.ncho@gmail.com>
2017-05-03 16:24:14 +0200

    modelisation du DAO et integration du moteur de template Twig

--- app/routes.php
+++ app/routes.php
@@ -1,14 +1,8 @@
 <?php
 
 // Home page
-$app->get('/', function () {
+$app->get('/', function () use ($app) {
+    $articles = $app['dao.article']->findAll();
     
-    require '../src/model.php';
-    $articles = getArticles();
-
-    ob_start();             // start buffering HTML output
-    
-    require '../views/view.php';
-    $view = ob_get_clean(); // assign HTML output to $view
-    return $view;
+    return $app['twig']->render('index.html.twig', array('articles' => $articles));
 });
--- src/model.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-function getArticles() {
-    $bdd = new PDO('mysql:host=localhost;dbname=microcms;charset=utf8', 'microcms_user', 'secret');
-    $articles = $bdd->query('SELECT * FROM t_article ORDER BY art_id DESC');
-    return $articles;
-}
